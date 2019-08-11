8aadaf267a5d2f4df41d7b2f6813a10ff708853e
nolanlad <nolanrmccarthy@gmail.com>
2019-08-11 13:43:20 -0700

    ND derivatives lookin' like a sncc

--- UniformSpace.py
+++ UniformSpace.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import linalg
+import itertools
 
 
 '''============================================================
@@ -40,6 +41,32 @@ def factorial(N):
 
 def n_combos(n,r):
     return factorial(n+r-1)/((factorial(r)*factorial(n-1)))
+
+def make_combos(shit,n):
+    shit = list(shit)
+    shit.extend([1])
+    return [multiply_els(c) for c in \
+            itertools.combinations_with_replacement(shit,n)]
+
+def make_combos2(shit2,n):
+    shit = list(shit2)
+    shit.append(' ')
+    return [sum_els(c).strip() for c in \
+            itertools.combinations_with_replacement(shit,n)]
+    
+
+def multiply_els(iterr):
+    acc = 1
+    for i in iterr:
+        acc*=i
+    return acc
+
+def sum_els(iterr):
+    acc = ''
+    for i in iterr:
+        acc+=i
+    return acc
+
 
 def get_p(N,h):
     p = ((2*((N+1)/2))-2 + h)/2
@@ -204,6 +231,7 @@ class NodeWalker:
         self.layer = [node]
         self.N = N
         self.variance_dim = None
+        self.count =0
 
     def __iter__(self):
         return self
@@ -212,7 +240,8 @@ class NodeWalker:
         '''
         Please someone who knows graph theory make this not suck
         '''
-        if len(self.visited) == self.N:
+        #if len(self.visited) == self.N:
+        if self.count == self.N-1:
             raise StopIteration()
         for n in self.layer:
             for n2 in n.neighbors:
@@ -220,9 +249,12 @@ class NodeWalker:
                     if self.variance_dim != None and (n2.get_val(self.variance_dim) -
                         self.startnode.get_val(self.variance_dim)) != 0:
                         self.visited.append(n2.N)
+                        self.count+=1
                         return n2
-                    #else:
-                     #   self.visited.append(n2.N)
+                    else:
+                        self.visited.append(n2.N)
+                        print(n2.N)
+                        #return n2
         newlayer = []
         for l in self.layer:
             newlayer.extend(l.neighbors)
@@ -233,9 +265,12 @@ class NodeWalker:
                 if n2.N not in self.visited:
                     if (n2.get_val(self.variance_dim) - self.startnode.get_val(self.variance_dim)) != 0:
                         self.visited.append(n2.N)
+                        self.count+=1
                         return n2
-                    #else:
-                    #    self.visited.append(n2.N)
+                    else:
+                        self.visited.append(n2.N)
+                        print('b')
+                        #return n2
                         
     def next(self):
         return self.__next__()
@@ -273,12 +308,98 @@ def nodesquare():
     for i in [0,1,2,3,4,5,6,7,8]:
         nodelist[4].link(nodelist[i])
     return nodelist
-        
+
+def nodesquare2():
+    space = Space()
+    space.add_dimension("x")
+    space.add_dimension("y")
+    #space.add_dimension("z")
+    nodelist = []
+    for i in range(-1,2):
+        for j in range(-1,2):
+            nodelist.append(space.nodegen([i,j]))
+    for i in [1,3,5,7]:
+        nodelist[4].link(nodelist[i])
+    return nodelist       
     
 
 def test1(node,dim1,dim2,N,h):
-    ne = node.get_neighbors(node)
-    #get diffs
-    #check invariability
-    #remove invariates
-    
+    ndims = len(node.refspace.dimnames)+1
+    dimms = tuple(node.refspace.dimnames)
+    p = int(n_combos(ndims,N+h))
+    walker = NodeWalker(node,p)
+    walker.add_variance_var(dim2)
+    diffs = []
+    nodes = [n for n in walker]
+    nodes.append(node)
+    for n in nodes:
+        diffs.append(n.get_vals(dimms) - node.get_vals(dimms) )
+    stiffness = np.zeros((int(p),int(p)))
+    for i in range(p):
+        stiffness[:,i] = make_combos(diffs[i],2)
+    #stiffness[p-1][p-1] = 1
+    print(make_combos2(node.refspace.dimnames,N+h))
+    print([n.N for n in nodes])
+    return stiffness
+
+def differentiate(node,dim2,N,h):
+    ndims = len(node.refspace.dimnames)+1
+    dimms = tuple(node.refspace.dimnames)
+    p = int(n_combos(ndims,N+h))
+    walker = NodeWalker(node,p)
+    walker.add_variance_var(dim2)
+    diffs = []
+    nodes = [n for n in walker]
+    nodes.append(node)
+    for n in nodes:
+        diffs.append(n.get_vals(dimms) - node.get_vals(dimms) )
+    stiffness = np.zeros((int(p),int(p)))
+    for i in range(p):
+        stiffness[:,i] = make_combos(diffs[i],N+h)
+    thelist = make_combos2(node.refspace.dimnames,N+h)
+    b = np.zeros(int(p))
+    b[thelist.index(dim2*N)] = factorial(N)
+    ids = [n.N for n in nodes]
+    soln = np.linalg.solve(stiffness,b)
+    return ids,soln
+
+def test1(node,dim1,dim2,N,h):
+    ndims = len(node.refspace.dimnames)+1
+    dimms = tuple(node.refspace.dimnames)
+    p = int(n_combos(ndims,N+h))
+    walker = NodeWalker(node,p)
+    walker.add_variance_var(dim2)
+    diffs = []
+    nodes = [n for n in walker]
+    nodes.append(node)
+    for n in nodes:
+        diffs.append(n.get_vals(dimms) - node.get_vals(dimms) )
+    stiffness = np.zeros((int(p),int(p)))
+    for i in range(p):
+        stiffness[:,i] = make_combos(diffs[i],2)
+    #stiffness[p-1][p-1] = 1
+    print(make_combos2(node.refspace.dimnames,N+h))
+    print([n.N for n in nodes])
+    return stiffness
+
+
+'''
+def test2(node,dim1,dim2,N,h):
+    ndims = len(node.refspace.dimnames)+1
+    dimms = tuple(node.refspace.dimnames)
+    p = int(n_combos(ndims,N+h))
+    print(p)
+    walker = NodeWalker(node,p)
+    #walker.add_variance_var(dim2)
+    diffs = []
+    nodes = [n for n in walker]
+    for n in nodes:
+        diffs.append(n.get_vals(dimms) - node.get_vals(dimms) )
+    stiffness = np.zeros((int(p),int(p)))
+    for i in range(p-1):
+        stiffness[:,i] = make_combos(diffs[i],1)
+    stiffness[0][p-1] = 1
+    print(make_combos2(node.refspace.dimnames,N+h))
+    print([n.id for n in nodes])
+    return stiffness
+'''
