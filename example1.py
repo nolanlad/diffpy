47263dea894f0f9e8aff3e6c61e4638795d184e6
nolanlad <nolanrmccarthy@gmail.com>
2020-03-17 15:35:00 -0700

    hello again

--- example1.py
+++ example1.py
@@ -5,6 +5,8 @@ heat generation of 1W/m^3
 
 from diff import *
 import matplotlib.pyplot as plt
+import scipy.sparse
+import scipy
 
 
 #thermal properties of Aluminum
@@ -12,13 +14,15 @@ k = 205.
 alpha = 9.7e-5
 Q = 1.0
 
-N = 25
+N = 10
+accuracy = 4
 x = np.sqrt(np.linspace(0,1,N))
 nods = nodelinspace_1D(x)
 A = np.zeros((N,N))
 
+
 for i in range(1,N-1):
-	ids,vals = nods[i].diff('x',2,0)
+	ids,vals = nods[i].diff('x',2,accuracy)
 	A[i][ids] += alpha*vals
 
 #constant temperature boundary condition
@@ -26,7 +30,8 @@ A[0][0] = 1
 #A[-1][-1] = 1
 
 #Adiabatic
-ids,vals = nods[-1].diff('x',1,0)
+ids,vals = nods[-1].diff('x',1,accuracy)
+A[-1] = 0
 A[-1][ids] = -k*vals
 
 #boundary conditions
@@ -37,6 +42,36 @@ b[-1] = 0
 T = np.linalg.solve(A,b)
 
 print('max temp = ', np.amax(T))
-plt.plot(x,T,'bo')
-plt.show()
+plt.plot(x,T,'b-')
+#plt.show()
 
+ids,vals = nods[0].diff('x',1,accuracy)
+A[0] = 0
+A[0][ids] =  -alpha*vals
+ids,vals = nods[-1].diff('x',1,accuracy)
+A[-1] = 0
+A[-1][ids] =  -alpha*vals
+
+A = scipy.sparse.csr_matrix(A)
+
+dt = 0.1
+T2 = T
+for i in range(10000):
+    #dT = dt*np.matmul(A,T2)
+    dT = dt*(A@T2)
+    T2 = T2 + dT
+    T2[0] = 300
+    T2[-1] = T2[-2]
+
+plt.plot(x,T2,'r-')
+
+for i in range(30000):
+    #dT = dt*np.matmul(A,T2)
+    dT = dt*(A@T2)
+    T2 = T2 + dT
+    T2[0] = 300
+    T2[-1] = T2[-2]
+
+print(np.amax(T2))
+
+plt.plot(x,T2,'g-')
