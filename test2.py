745aa29407103fc2bb1e4f1a767defda81ba6376
nolanlad <nolanrmccarthy@gmail.com>
2021-02-14 14:22:06 -0500

    cleanup

--- test2.py
+++
@@ -1,9 +0,0 @@
-from diff import *
-import matplotlib.pyplot as plt
-nodd = node_rect()
-center = nodd[5][5]
-nodd2 = []
-for n in nodd:
-    nodd2.extend(n)
-
-nodd = nodd2
--- test7.py
+++
@@ -1,8 +0,0 @@
-from diff import *
-import matplotlib.pyplot as plt
-
-N = 25
-x = np.sqrt(np.linspace(0,1,N))
-nods = nodelinspace_1D(x)
-
-y = x**2
